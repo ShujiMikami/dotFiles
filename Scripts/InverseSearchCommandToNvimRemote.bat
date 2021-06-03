7bd1858214c2a79528ac46baebfff312b6ac74d3
CO\MM07860 <shuji_mikami@murata.com>
2021-06-03 20:04:46 +0900

    コマンドによる行飛ばしが正常に動くように

--- Scripts/InverseSearchCommandToNvimRemote.bat
+++ Scripts/InverseSearchCommandToNvimRemote.bat
@@ -1,2 +1,2 @@
 for /F %%i in ('type %TEMP%\nvimsocket') do set SERVERNAME=%%i
-%HOME%\.pyenv\pyenv-win\versions\nvim-python3\Scripts\nvr --servername %SERVERNAME% --remote-send "%lG"
+%HOME%\.pyenv\pyenv-win\versions\nvim-python3\Scripts\nvr --servername %SERVERNAME% --remote-send "%1G"

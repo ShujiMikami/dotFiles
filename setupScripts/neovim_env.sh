7e3883f90a91e2008d76c4cefec7e5a6dc770989
ShujiMikami <carbonized.pan@gmail.com>
2022-03-22 10:54:51 +0900

    ファイルの移動を反映

--- setupScripts/neovim_env.sh
+++ setupScripts/neovim_env.sh
@@ -5,10 +5,10 @@ CURRENT_DIR=$(cd $(dirname $0) && pwd)
 ln -s $CURRENT_DIR/neovim/nvim ~/.config/nvim
 
 # neovim/nvim-python3を.cacheにコピー
-cp -r neovim/nvim-python3 ~/.cache/ 
+cp -r ../neovim/nvim-python3 ~/.cache/ 
 
 # neovim/nvim-python2を.cacheにコピー
-cp -r neovim/nvim-python2 ~/.cache/ 
+cp -r ../neovim/nvim-python2 ~/.cache/ 
 
 # pipenvのpipファイル直下に仮想環境を作成する設定
 export PIPENV_VENV_IN_PROJECT=1

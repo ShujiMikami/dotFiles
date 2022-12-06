728105fd2f63e8689d965c589d722de95d66ade8
shuji_mikami <carbonized.pan@gmail.com>
2022-12-06 18:26:38 +0900

    defaultsを追記

--- setupScripts/ChildScripts/defaults.sh
+++ setupScripts/ChildScripts/defaults.sh
@@ -5,9 +5,27 @@ if [ "$(uname)" != "Darwin" ]; then
   exit 1
 fi
 
-# ====================Input source=============================
+# ====================General===========================
+# Default browser Google chrome
+defaults write com.google.Chrome LastRunAppBundlePath -string "/Applications/Google Chrome.app"
+
+# ====================Keyboard=============================
 # Disable auto-capitalization
 defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+
+# Use fn key
+defaults write NSGlobalDomain com.apple.keyboard.fnState -bool true
+
+# Disable live conversion
+defaults write com.apple.inputmethod.Kotoeri JIMPrefLiveConversionKey -bool false
+
+# ====================Sound================================
+# Visible enable Sound
+defaults write com.apple.controlcenter NSStatusItem Visible Sound -bool true
+
+# ====================Bluetooth============================
+# Visible enable Bluetooth 
+defaults write com.apple.controlcenter NSStatusItem Visible Bluetooth -bool true
 
 # ====================Finder==============================-
 # Show hidden files by defaults
@@ -25,9 +43,6 @@ defaults write com.apple.dock auto-hide -bool true
 
 # Disable show recents
 defaults write com.apple.dock show-recents -bool false
-
-
-
 
 # Reflect setting
 killall Dock

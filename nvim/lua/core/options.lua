a066fd321074552798bffaa0408747a4e2c90ab9
ShujiMikami <carbonized.pan@gmail.com>
2026-05-25 09:12:36 +0900

    neovimを開始

---
+++ nvim/lua/core/options.lua
@@ -0,0 +1,13 @@
+vim.opt.number = true
+vim.opt.relativenumber = false
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.termguicolors = true
+
+vim.opt.clipboard = "unnamedplus"

dd80ec925f07d0d9c5b8644be2dd477725c2e609
ShujiMikami <carbonized.pan@gmail.com>
2026-06-06 19:18:49 +0900

    add lualine

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+
+    opts = {
+      options = {
+        theme = "tokyonight",
+        globalstatus = true,
+      },
+
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { "filename" },
+
+        lualine_x = { "encoding", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+    },
+  },
+}

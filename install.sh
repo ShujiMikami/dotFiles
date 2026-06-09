c89e62736c41a32a47734dd7ec3377e86c63269e
ShujiMikami <carbonized.pan@gmail.com>
2026-06-10 00:20:39 +0900

    template, docs, install, Brewfile

---
+++ install.sh
@@ -0,0 +1,14 @@
+#/usr/bin/env bash
+
+set -euo pipefail
+
+DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+mkdir -p "$HOME/.config"
+
+ln -sfn "$DOTFILES_DIR/.config/nvim" "$HOME/.config/nvim"
+
+ln -sf "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc"
+ln -sf "$DOTFILES_DIR/.gitconfig" "$HOME/.gitconfig"
+
+echo "dotfiles installed"

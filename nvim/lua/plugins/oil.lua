81b1dbd42144f5102ca1630f54592b5c56cd087e
ShujiMikami <carbonized.pan@gmail.com>
2026-06-21 04:40:24 +0900

    fix oil

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,11 +1,14 @@
 return {
   "stevearc/oil.nvim",
-  opts = {},
+  opts = {
+    default_file_explorer = true,
+    view_options = {
+      show_hidden = true,
+    },
+  },
+
   dependencies = {
     "nvim-tree/nvim-web-devicons"
-  },
-  view_options = {
-    show_hidden = true,
   },
   lazy = false,
   keys = {

23d0acdb6097b2aca9924ff4c393dca009c1c81c
ShujiMikami <carbonized.pan@gmail.com>
2026-06-21 03:17:25 +0900

    add oil, fix lsp keymap

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -4,10 +4,3 @@ local keymap = vim.keymap
 
 keymap.set("n", "<leader>w", ":w<CR>")
 keymap.set("n", "<leader>q", ":q<CR>")
-
-keymap.set("n", "gd", vim.lsp.buf.definition, {
-  desc = "Goto Definition"
-})
-keymap.set("n", "K", vim.lsp.buf.hover, {
-  desc = "Hover"
-})
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,15 +1,27 @@
-vim.diagnostic.config({
-  virtual_text=true,
-  signs = true,
-  underline = true,
-  update_in_insert = false,
-  severity_sort = true,
-})
-
 return {
   {
     "neovim/nvim-lspconfig",
 
+    opts = {
+      diagnostics = {
+        virtual_text=true,
+        signs = true,
+        underline = true,
+        update_in_insert = false,
+        severity_sort = true,
+      },
+    },
+    servers = {
+      ['*'] = {
+        keys = {
+          -- jump to definition
+          { "gd", vim.lsp.buf.definition, desc = "Goto Definition" },
+
+          -- hover
+          { "K", vim.lsp.buf.hover, desc = "Hover" },
+        },
+      }
+    },
     config = function()
       require("lsp.clangd")
     end,
